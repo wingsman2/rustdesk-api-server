2e4546a682ddb4609f95ecde5333041f12088a3a
wingsman2 <blah_1212@hotmail.com>
2024-02-20 18:02:56 -0500

    Update models_user.py

--- api/models_user.py
+++ api/models_user.py
@@ -1,7 +1,7 @@
 # cython:language_level=3
 from django.db import models
 from django.contrib.auth.models import (
-    BaseUserManager,AbstractBaseUser,PermissionsMixin
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
 )
 from .models_work import *
 
@@ -9,7 +9,7 @@ from .models_work import *
 class MyUserManager(BaseUserManager):
     def create_user(self, username, password=None):
         if not username:
-            raise ValueError('Users must have an username')
+            raise ValueError('Users must have a username')
 
         user = self.model(username=username,
         )
@@ -29,37 +29,37 @@ class MyUserManager(BaseUserManager):
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField('用户名', 
+    username = models.CharField('Username', 
                                 unique=True,
                                 max_length=50)
     
     rid = models.CharField(verbose_name='RustDesk ID', max_length=16)
     uuid = models.CharField(verbose_name='uuid', max_length=60)
-    autoLogin = models.BooleanField(verbose_name='autoLogin', default=True)
-    rtype = models.CharField(verbose_name='rtype', max_length=20)
-    deviceInfo = models.TextField(verbose_name='登录信息:', blank=True)
+    autoLogin = models.BooleanField(verbose_name='Auto Login', default=True)
+    rtype = models.CharField(verbose_name='Type', max_length=20)
+    deviceInfo = models.TextField(verbose_name='Login Information:', blank=True)
     
-    is_active = models.BooleanField(verbose_name='是否激活', default=True)
-    is_admin = models.BooleanField(verbose_name='是否管理员', default=False)
+    is_active = models.BooleanField(verbose_name='Active', default=True)
+    is_admin = models.BooleanField(verbose_name='Admin', default=False)
 
     objects = MyUserManager()
  
-    USERNAME_FIELD = 'username'  # 用作用户名的字段
-    REQUIRED_FIELDS = ['password']     #必须填写的字段
+    USERNAME_FIELD = 'username'  # Field used as username
+    REQUIRED_FIELDS = ['password']  # Fields that must be filled in
     
     
     def get_full_name(self):
-        # The user is identified by their email address
+        # The user is identified by their username
         return self.username
  
     def get_short_name(self):
-        # The user is identified by their email address
+        # The user is identified by their username
         return self.username
  
-    def __str__(self):              # __unicode__ on Python 2
+    def __str__(self):  # __unicode__ on Python 2
         return self.username
  
-    def has_perm(self, perm, obj=None):    #有没有指定的权限
+    def has_perm(self, perm, obj=None):  # Does the user have the specified permission?
         "Does the user have a specific permission?"
         # Simplest possible answer: Yes, always
         return True
@@ -79,11 +79,10 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     class Meta:
     
-        verbose_name = "用户"
-        verbose_name_plural = "用户列表"
+        verbose_name = "User"
+        verbose_name_plural = "User List"
         permissions = (
             ("view_task", "Can see available tasks"),
             ("change_task_status", "Can change the status of tasks"),
             ("close_task", "Can remove a task by setting its status as closed"),
         )
-
