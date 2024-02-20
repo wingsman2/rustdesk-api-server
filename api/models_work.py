b8f15190aac8561a4d790afb219515c37008e8d5
wingsman2 <blah_1212@hotmail.com>
2024-02-20 18:03:23 -0500

    Update models_work.py

--- api/models_work.py
+++ api/models_work.py
@@ -6,35 +6,35 @@ from django.contrib import admin
 class RustDeskToken(models.Model):
     ''' Token
     '''
-    username = models.CharField(verbose_name='用户名', max_length=20)
+    username = models.CharField(verbose_name='Username', max_length=20)
     rid = models.CharField(verbose_name='RustDesk ID', max_length=16)
-    uid = models.CharField(verbose_name='用户ID', max_length=16)
+    uid = models.CharField(verbose_name='User ID', max_length=16)
     uuid = models.CharField(verbose_name='uuid', max_length=60)
     access_token = models.CharField(verbose_name='access_token', max_length=60, blank=True)
-    create_time = models.DateTimeField(verbose_name='登录时间', auto_now_add=True)
-    #expire_time = models.DateTimeField(verbose_name='过期时间')
+    create_time = models.DateTimeField(verbose_name='Login Time', auto_now_add=True)
+    #expire_time = models.DateTimeField(verbose_name='Expiration Time')
     class Meta:
         ordering = ('-username',)
         verbose_name = "Token"
-        verbose_name_plural = "Token列表" 
+        verbose_name_plural = "Token List" 
 
 class RustDeskTokenAdmin(admin.ModelAdmin):
     list_display = ('username', 'uid')
     search_fields = ('username', 'uid')
-    list_filter = ('create_time', ) #过滤器
+    list_filter = ('create_time', ) # Filter
     
 
 class RustDeskTag(models.Model):
     ''' Tags
     '''
-    uid = models.CharField(verbose_name='所属用户ID', max_length=16)
-    tag_name = models.CharField(verbose_name='标签名称', max_length=60)
-    tag_color = models.CharField(verbose_name='标签颜色', max_length=60, blank=True)
+    uid = models.CharField(verbose_name='Owner User ID', max_length=16)
+    tag_name = models.CharField(verbose_name='Tag Name', max_length=60)
+    tag_color = models.CharField(verbose_name='Tag Color', max_length=60, blank=True)
     
     class Meta:
         ordering = ('-uid',)
         verbose_name = "Tags"
-        verbose_name_plural = "Tags列表"
+        verbose_name_plural = "Tags List"
 
 class RustDeskTagAdmin(admin.ModelAdmin):
     list_display = ('tag_name', 'uid', 'tag_color')
@@ -43,21 +43,21 @@ class RustDeskTagAdmin(admin.ModelAdmin):
     
 
 class RustDeskPeer(models.Model):
-    ''' Pees
+    ''' Peers
     '''
-    uid = models.CharField(verbose_name='用户ID', max_length=16)
-    rid = models.CharField(verbose_name='客户端ID', max_length=60)
-    username = models.CharField(verbose_name='系统用户名', max_length=20)
-    hostname = models.CharField(verbose_name='操作系统名', max_length=30)
-    alias = models.CharField(verbose_name='别名', max_length=30)
-    platform = models.CharField(verbose_name='平台', max_length=30)
-    tags = models.CharField(verbose_name='标签', max_length=30)
-    rhash = models.CharField(verbose_name='设备链接密码', max_length=60)
+    uid = models.CharField(verbose_name='User ID', max_length=16)
+    rid = models.CharField(verbose_name='Client ID', max_length=60)
+    username = models.CharField(verbose_name='System Username', max_length=20)
+    hostname = models.CharField(verbose_name='Operating System Name', max_length=30)
+    alias = models.CharField(verbose_name='Alias', max_length=30)
+    platform = models.CharField(verbose_name='Platform', max_length=30)
+    tags = models.CharField(verbose_name='Tags', max_length=30)
+    rhash = models.CharField(verbose_name='Device Link Password', max_length=60)
     
     class Meta:
         ordering = ('-username',)
         verbose_name = "Peers"
-        verbose_name_plural = "Peers列表" 
+        verbose_name_plural = "Peers List" 
         
 
 class RustDeskPeerAdmin(admin.ModelAdmin):
@@ -67,21 +67,21 @@ class RustDeskPeerAdmin(admin.ModelAdmin):
     
     
 class RustDesDevice(models.Model):
-    rid = models.CharField(verbose_name='客户端ID', max_length=60, blank=True)
+    rid = models.CharField(verbose_name='Client ID', max_length=60, blank=True)
     cpu = models.CharField(verbose_name='CPU', max_length=20)
-    hostname = models.CharField(verbose_name='主机名', max_length=20)
-    memory = models.CharField(verbose_name='内存', max_length=20)
-    os = models.CharField(verbose_name='操作系统', max_length=20)
+    hostname = models.CharField(verbose_name='Hostname', max_length=20)
+    memory = models.CharField(verbose_name='Memory', max_length=20)
+    os = models.CharField(verbose_name='Operating System', max_length=20)
     uuid = models.CharField(verbose_name='uuid', max_length=60)
-    username = models.CharField(verbose_name='系统用户名', max_length=60, blank=True)
-    version = models.CharField(verbose_name='客户端版本', max_length=20)
-    create_time = models.DateTimeField(verbose_name='设备注册时间', auto_now_add=True)
-    update_time = models.DateTimeField(verbose_name='设备更新时间', auto_now=True, blank=True)
+    username = models.CharField(verbose_name='System Username', max_length=60, blank=True)
+    version = models.CharField(verbose_name='Client Version', max_length=20)
+    create_time = models.DateTimeField(verbose_name='Device Registration Time', auto_now_add=True)
+    update_time = models.DateTimeField(verbose_name='Device Update Time', auto_now=True, blank=True)
     
     class Meta:
         ordering = ('-rid',)
-        verbose_name = "设备"
-        verbose_name_plural = "设备列表" 
+        verbose_name = "Device"
+        verbose_name_plural = "Device List" 
     
 class RustDesDeviceAdmin(admin.ModelAdmin):
     list_display = ('rid', 'hostname', 'memory', 'uuid', 'version', 'create_time', 'update_time')
@@ -91,21 +91,21 @@ class RustDesDeviceAdmin(admin.ModelAdmin):
 
 
 class ShareLink(models.Model):
-    ''' 分享链接
+    ''' Share Link
     '''
-    uid = models.CharField(verbose_name='用户ID', max_length=16)
-    shash = models.CharField(verbose_name='链接Key', max_length=60)
-    peers = models.CharField(verbose_name='机器ID列表', max_length=20)
-    is_used = models.BooleanField(verbose_name='是否使用', default=False)
-    is_expired = models.BooleanField(verbose_name='是否过期', default=False)
-    create_time = models.DateTimeField(verbose_name='生成时间', auto_now_add=True)
+    uid = models.CharField(verbose_name='User ID', max_length=16)
+    shash = models.CharField(verbose_name='Link Key', max_length=60)
+    peers = models.CharField(verbose_name='Machine ID List', max_length=20)
+    is_used = models.BooleanField(verbose_name='Used', default=False)
+    is_expired = models.BooleanField(verbose_name='Expired', default=False)
+    create_time = models.DateTimeField(verbose_name='Generation Time', auto_now_add=True)
     
 
     
     class Meta:
         ordering = ('-create_time',)
-        verbose_name = "分享链接"
-        verbose_name_plural = "链接列表" 
+        verbose_name = "Share Link"
+        verbose_name_plural = "Link List" 
         
 
 class ShareLinkAdmin(admin.ModelAdmin):
